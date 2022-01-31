c110fc2f84ee1c6a5e51f5ee95f0d5be95bf8771
Thao-Tran Le-Phuong <thaotran.lp@gmail.com>
2022-01-30 23:03:57 -0500

    Create models module

---
+++ menuapi/src/models.rs
@@ -0,0 +1,53 @@
+use ring::digest;
+use serde::{Deserialize, Serialize};
+use std::num::NonZeroU32;
+use uuid::Uuid;
+
+const HASH_LEN: usize = digest::SHA256_OUTPUT_LEN;
+pub type PasswordHash = [u8; HASH_LEN];
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct UserReq {
+  pub email: String,
+  pub password: String,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Clone)]
+pub struct User {
+  pub id: Uuid,
+  pub email: String,
+  pub hash: PasswordHash,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Clone)]
+pub struct Token {
+  pub id: Uuid,
+  pub token: String,
+}
+
+#[derive(Deserialize, Clone)]
+pub struct Settings {
+  pub salt_iterations: NonZeroU32,
+  pub salt: String,
+  pub jwt_secret: String,
+}
+
+#[derive(Debug)]
+pub enum AuthError {
+  JwtError(jwt::error::Error),
+  WrongUsernameOrPassword,
+  ExpiredToken,
+  InvalidToken,
+}
+
+impl warp::reject::Reject for AuthError {}
+
+#[derive(Debug)]
+pub enum HandlerError {
+  Auth(AuthError),
+  Db(rusqlite::Error),
+  Conflict,
+  BadRequest,
+}
+
+impl warp::reject::Reject for HandlerError {}
